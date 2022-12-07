5576853e62850e5d1f7afb4b01a71c00f6de8ae8
Sebastiaan Marynissen <sebastiaan.marynissen@gmail.com>
2022-12-07 12:31:01 +0100

    Fix typo

--- test/some-test.js
+++ test/some-test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-describe('This is a test suit', function() {
+describe('This is a test suite', function() {
 
 	it('performs a test', function() {
 		expect('foo').to.not.equal('bar');
